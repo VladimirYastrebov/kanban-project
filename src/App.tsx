4aa811bfa1ee78b4d7b7b5db34daab8ac057f868
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-03-14 14:35:39 +0300

    added dargndDrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="container mx-auto p-8">
+        <h1 className="text-2xl font-bold mb-4">Vite + shadcn/ui</h1>
+        <Button>Get started</Button>
+    </div>
+  );
 }
-export default App
+export default App;
